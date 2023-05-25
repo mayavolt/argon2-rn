9803b34fe58dbf5a8dea1492482b9f795577bc49
Maya <81964044+mayavolt@users.noreply.github.com>
2023-05-25 22:07:03 +0300

    Update RNArgon2.swift

--- ios/RNArgon2.swift
+++ ios/RNArgon2.swift
@@ -7,12 +7,16 @@ class RNArgon2: NSObject {
   static func requiresMainQueueSetup() -> Bool {
     return true
   }
-
+  
+  // Method for generating Argon2 hash
   @objc
   func argon2(_ password: String, salt: String, config: NSDictionary? = nil, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) -> Void {
+    
+    // Initialize Argon2 context & convert config dictionary to Swift Dictionary
     let argon2Context = CatArgon2Context.init();
     let configDict = config as! Dictionary<String,Any>
 
+    // Set Argon2 context properties based on the config values
     argon2Context.iterations = configDict["iterations", default: 2 ] as! Int;
     argon2Context.memory = configDict["memory", default: 32 * 1024 ] as! Int;
     argon2Context.parallelism = configDict["parallelism", default: 1 ] as! Int;
@@ -20,28 +24,37 @@ class RNArgon2: NSObject {
     argon2Context.hashLength = configDict["hashLength", default: 32 ] as! Int;
     argon2Context.mode = getArgon2Mode(mode: configDict["mode", default: "argon2id" ] as! String);
 
+    // Initialize Argon2 crypto with the context
     let argon2Crypto = CatArgon2Crypto.init(context: argon2Context);
+    
+    // Generate encoded and raw Argon2 hashes
     let encodedResult = argon2Crypto.hash(password: password);
+    let rawResult = argon2Crypto.hash(password: password);
 
     // Set the hasher to hash as raw since encoded is the default
     argon2Crypto.context.hashResultType = .hashRaw;
-    let rawResult = argon2Crypto.hash(password: password);
-
+    
+    // Check for errors in hash generation
     if ((rawResult.error) != nil || (encodedResult.error) != nil) {
-        let error = NSError(domain: "com.poowf.argon2", code: 200, userInfo: ["Error reason": "Failed to generate argon2 hash"])
-        reject("E_ARGON2", "Failed to generate argon2 hash", error)
+        let error = NSError(domain: "com.argon2.rn", code: 200, userInfo: ["Error reason": "Failed to generate argon2 hash"])
+        reject("E_ARGON2", "Failed to generate Argon2 hash", error)
     }
 
+    // Get the hexadecimal representation of raw and encoded hashes
     let rawHash = rawResult.hexStringValue();
     let encodedHash = encodedResult.stringValue();
 
+    // Create a dictionary with the hash results
     let resultDictionary: NSDictionary = [
         "rawHash" : rawHash,
         "encodedHash" : encodedHash,
     ]
+    
+    // Resolve the Promise with the result dictionary
     resolve(resultDictionary);
   }
 
+  // Get the Argon2 mode based on the provided string
   func getArgon2Mode(mode: String) -> CatArgon2Mode {
     var selectedMode: CatArgon2Mode;
     switch mode {
@@ -58,7 +71,6 @@ class RNArgon2: NSObject {
             selectedMode = CatArgon2Mode.argon2id;
             break;
     }
-
     return selectedMode;
   }
 }
